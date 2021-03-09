3c770e5e3db3466b9fa9c4d08e153501a4f45368
JounKim <52889174+JounKim@users.noreply.github.com>
2021-03-09 23:13:42 +0900

    Create Human.cs

---
+++ CSHARP/CSHARP/Human.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c샵책예제
+{
+    class Human
+    {
+        public string name = "name";
+        public float height = 165;
+        public int age = 10;
+
+        public void Eat()
+        {
+            height += 10;
+        }
+
+        public void Walk()
+        {
+            height -= 5;
+        }
+
+        public void Sleep()
+        {
+            age++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your character's name: ");
+            string CharName = Console.ReadLine();
+            Console.Write("Enter your character's age: ");
+            int CharAge = int.Parse(Console.ReadLine());
+            Console.Write("Enter your character's height: ");
+            float CharHeight = float.Parse(Console.ReadLine());
+
+            Human Player = new Human();
+
+            Player.name = CharName;
+            Player.age = CharAge;
+            Player.height = CharHeight;
+            Player.Eat();
+            Player.Walk();
+            Player.Sleep();
+
+            Console.WriteLine("name {0}, age: {1}, height: {2}", Player.name, Player.age, Player.height);
+
+
+        }
+    }
+}
